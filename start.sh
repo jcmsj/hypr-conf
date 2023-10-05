c6c4ba7fe4f7bbc59163257ff93ea788cc408303
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2023-10-05 22:49:59 +0800

    defer eww scripts to the end, add firefox profile switcher

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-eww daemon
-eww open bar
+eww daemon &
+eww open bar &
 hyprpaper &
 wl-paste --type text --watch cliphist store & #Stores only text
 wl-paste --type image --watch cliphist store & #Stores only image data
 #waybar &
 nm-applet --indicator &
-mako
+mako &
+~/.config/eww/scripts/update-brightness &
+~/.config/eww/scripts/update-vol &
+~/.config/eww/scripts/bt/update
