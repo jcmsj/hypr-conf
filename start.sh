a80c873627971c69b432cd5c7420cc6ea79ad453
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2024-03-22 21:49:12 +0800

    Feat: sleep and autolock

--- start.sh
+++ start.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
+# numbers represent the non-parallel orders too
+#1 lockscreen and wallpaper
+hyprlock & 
+hyprpaper &
+#2 eww
 eww daemon &
 eww open bar &
-hyprpaper &
-wl-paste --type text --watch cliphist store & #Stores only text
-wl-paste --type image --watch cliphist store & #Stores only image data
-#waybar &
+eww open barx & 
 nm-applet --indicator &
-mako &
+
+#3
 ~/.config/eww/scripts/update-brightness &
 ~/.config/eww/scripts/update-vol &
-~/.config/eww/scripts/bt/update
+~/.config/eww/scripts/bt/update &
+
+#4 clipboards
+wl-paste --type text --watch cliphist store & #Stores only text
+wl-paste --type image --watch cliphist store & #Stores only image data
+
+#5
+hypridle ## dont run hypridle concurrently
