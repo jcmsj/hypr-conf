fbcf847881026ed3405370e50cbe51d3bf9eec7c
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2024-04-02 22:51:29 +0800

    Fix: nm-applet not starting; Feat: toggle power menu w/ ALT+F4 or CTRL+ALT+DEL

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ hyprpaper &
 #2 auto starts the gnome's auth agent 
 $gnomeAuthAgent &
 #3 ags
-ags
+ags &
 nm-applet --indicator &
 
 #4 clipboards
@@ -14,4 +14,4 @@ wl-paste --type text --watch cliphist store & #Stores only text
 wl-paste --type image --watch cliphist store & #Stores only image data
 
 #5
-hypridle ## dont run hypridle concurrently
+hypridle
