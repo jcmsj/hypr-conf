640f92237421867820c0f8e16db88dd970f43eda
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2024-04-09 09:58:47 +0800

    fix restore script

--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 path=/tmp/last_monitor_mode
-lastMode=cat < $path
-monitorCount=hyprctl monitors -j | jq length
+lastMode=$(cat < $path)
+monitorCount=$(hyprctl monitors -j | jq length)
 
 # if only one monitor then do nothing
 if [ $monitorCount -eq 1 ]; then
